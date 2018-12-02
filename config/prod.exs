2dc3ae32b08f59a299faed2e092a9bd040dd067f
Anna Anks Nowak <matisnape@wp.pl>
2018-12-02 16:21:37 +0100

    Add adapter for EbooklyPhoenix.Repo

--- config/prod.exs
+++ config/prod.exs
@@ -21,6 +21,7 @@ config :ebookly_phoenix, EbooklyPhoenixWeb.Endpoint,
 
 # Configure your database
 config :ebookly_phoenix, EbooklyPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
