5bf415b66ec9ef51483102cbd1dc955b36ac4aa7
Anna Anks Nowak <matisnape@wp.pl>
2018-12-02 16:57:41 +0100

    rename app

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :ebookly_phoenix, EbooklyPhoenixWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "secret-island-28575.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "ebooklyphoenix.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
