08f94e253ed7c9cf192d0a508ca2fbbcc2d05f05
Anna Anks Nowak <matisnape@wp.pl>
2018-12-02 16:16:03 +0100

    Oopsie - fix for misconfig

--- config/prod.exs
+++ config/prod.exs
@@ -20,7 +20,7 @@ config :ebookly_phoenix, EbooklyPhoenixWeb.Endpoint,
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Configure your database
-config :hello, Hello.Repo,
+config :ebookly_phoenix, EbooklyPhoenix.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
